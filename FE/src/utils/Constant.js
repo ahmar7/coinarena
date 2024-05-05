16f99a46fe0315e8cb57c6c26093a246f11a61dd
ahmar7 <ahmarjabbar7@gmail.com>
2024-05-06 00:34:14 +0500

    ne2

--- FE/src/utils/Constant.js
+++ FE/src/utils/Constant.js
@@ -1,9 +1,9 @@
 module.exports = {
   // Local server
-  // baseUrl: "http://localhost:4000/api/v1",
+  baseUrl: "http://localhost:4000/api/v1",
   // Local server
   // Official server
 
-  baseUrl: "https://coinarena.onrender.com/api/v1",
+  // baseUrl: "https://coinarena.onrender.com/api/v1",
   // Official server
 };
